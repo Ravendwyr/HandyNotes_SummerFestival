f444ea1a2b50caf1df9ded419594f65d598de55a
Ethan Centaurai <stevenblanchard86@gmail.com>
2012-12-14 15:00:48 +0000

    Plumbing.

---
+++ Alliance.lua
@@ -0,0 +1,25 @@
+
+if UnitFactionGroup("player") ~= "Alliance" then return end
+
+
+local _, SummerFestival = ...
+local points = SummerFestival.points -- points[<mapfile>] = { [<coordinates>] = <quest ID> }
+
+
+-- Eastern Kingdoms
+
+
+-- Kalimdor
+
+
+-- Outland
+
+
+-- Northrend
+
+
+-- Cataclysm
+
+
+-- Pandaria
+
---
+++ Horde.lua
@@ -0,0 +1,25 @@
+
+if UnitFactionGroup("player") ~= "Horde" then return end
+
+
+local _, SummerFestival = ...
+local points = SummerFestival.points -- points[<mapfile>] = { [<coordinates>] = <quest ID> }
+
+
+-- Eastern Kingdoms
+
+
+-- Kalimdor
+
+
+-- Outland
+
+
+-- Northrend
+
+
+-- Cataclysm
+
+
+-- Pandaria
+
