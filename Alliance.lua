8963cebb493591d8511e6e37a8a1fa57a4e79d11
Ethan Centaurai <ethancentaurai@outlook.com>
2013-06-21 21:45:35 +0100

    Added Pandaria bonfire data.

--- Alliance.lua
+++ Alliance.lua
@@ -4,7 +4,7 @@ if UnitFactionGroup("player") ~= "Alliance" then return end
 
 local _, SummerFestival = ...
 local points = SummerFestival.points
--- points[<mapfile>] = { [<coordinates>] = { <quest ID>, <achievement ID>, <achievement criteria ID> } }
+-- points[<mapfile>] = { [<coordinates>] = "<questID>:<type>" }
 
 
 ----------------------
@@ -35,3 +35,31 @@ local points = SummerFestival.points
 --------------
 -- Pandaria --
 --------------
+points["DreadWastes"] = {
+	[56006940] = "32497:B",	-- Soggy's Gamble
+}
+
+points["Krasarang"] = {
+	[73900930] = "32499:B",	-- Zhu's Watch
+}
+
+points["KunLaiSummit"] = {
+	[71109090] = "32500:B",	-- Binan Village
+}
+
+points["TheJadeForest"] = {
+	[47104700] = "32498:B",	-- Dawn's Blossom
+}
+
+points["TownlongWastes"] = {
+	[71405630] = "32501:B",	-- Longying Outpost
+}
+
+points["ValeofEternalBlossoms"] = {
+	[77763400] = "32496:D",
+	[79683727] = "32510:H",
+}
+
+points["ValleyoftheFourWinds"] = {
+	[51805120] = "32502:B",	-- Halfhill
+}
