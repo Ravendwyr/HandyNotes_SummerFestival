c53c85fbc452320317f6e357523866d0a303d2f9
Ethan Centaurai <ethancentaurai@outlook.com>
2019-02-11 19:34:44 +0000

    Use server time instead of local time.
    Local time causes more problems than it solves.

--- Core.lua
+++ Core.lua
@@ -24,6 +24,7 @@ local C_Timer_NewTicker = _G.C_Timer.NewTicker
 local C_Calendar = _G.C_Calendar
 local GameTooltip = _G.GameTooltip
 local GetAchievementCriteriaInfo = _G.GetAchievementCriteriaInfo
+local GetGameTime = _G.GetGameTime
 local GetQuestsCompleted = _G.GetQuestsCompleted
 local gsub = _G.string.gsub
 local IsControlKeyDown = _G.IsControlKeyDown
@@ -285,16 +286,14 @@ local function CheckEventActive()
 		local event = C_Calendar.GetDayEvent(monthOffset, day, i)
 
 		if event.iconTexture == 235472 or event.iconTexture == 235473 or event.iconTexture == 235474 then
-			if event.sequenceType == "ONGOING" then
-				setEnabled = true
-			else
-				local hour = tonumber(date("%H"))
+			local hour, minute = GetGameTime()
 
-				if event.sequenceType == "END" and hour <= event.endTime.hour or event.sequenceType == "START" and hour >= event.startTime.hour then
-					setEnabled = true
-				else
-					setEnabled = false
-				end
+			setEnabled = event.sequenceType == "ONGOING" -- or event.sequenceType == "INFO"
+
+			if event.sequenceType == "START" then
+				setEnabled = hour >= event.startTime.hour and (hour > event.startTime.hour or minute >= event.startTime.minute)
+			elseif event.sequenceType == "END" then
+				setEnabled = hour <= event.endTime.hour and (hour < event.endTime.hour or minute <= event.endTime.minute)
 			end
 		end
 	end
